2e7099a8d4947f2c1b4715c7f223eb1a5664e000
kobi <kobiokiti@gmail.com>
2023-03-21 10:48:36 +0100

    Edited and altered cell with python

---
+++ app.py
@@ -0,0 +1,12 @@
+import openpyxl as xl
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']
+
+for row in range(1, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * int(0.9)
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save("transactions2.xlsx")
