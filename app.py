c197f89ce9323edf63216e6b8ae0be83017e453b
kobi <kobiokiti@gmail.com>
2023-03-21 22:16:11 +0100

    Cleared error, working perfectly

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
 cell = sheet['a1']
 
-for row in range(1, sheet.max_row + 1):
+for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
-    corrected_price = cell.value * int(0.9)
+    corrected_price = cell.value * 0.9
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
+
 
 wb.save("transactions2.xlsx")
